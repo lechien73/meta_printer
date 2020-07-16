c5ac6ef326af61b4e624ce568ec517743b7d22d9
Matt Rudge <mrudge@gmail.com>
2020-07-16 09:49:22 +0000

    Initial app commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# meta_printer
-Simple server to allow students to see the metadata from their projects
+# Metadata Printer
+
+Simple server to allow students to see the metadata from their projects.
+
+Written in Flask and using requests_html to acquire the metadata.
+
+## Usage
+
+Supply the URL of the live server followed by the URL you wish to check, for example:
+
+https://ci-metaprinter.herokuapp.com/https://mattrudge.net
+
+If everything works correctly, you will be returned to a page with a list of the `meta` element attributes
+
+----------
+Matt Rudge
+
+July, 2020
---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+
+from requests_html import HTMLSession
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/<path:url>")
+def index(url):
+    if url:
+        s = HTMLSession()
+        r = s.get(url)
+        results = r.html.find("meta")
+        r_list = [result.attrs for result in results]
+        return render_template("index.html", results=r_list)
+    else:
+        return render_template("error.html")
+
+
+@app.errorhandler(404)
+@app.errorhandler(405)
+def general_error(e):
+    """404 & 405 page custom error handler
+    Returns the general.html template and 405 status
+    """
+
+    return render_template("error.html", error=e), e.code
+
+
+if __name__ == "__main__":
+    app.run(host=os.getenv("IP", "0.0.0.0"), port=int(
+        os.getenv("PORT", "5000")), debug=False)
